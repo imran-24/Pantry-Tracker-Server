203089f1fd4622f51a1da81e2148b6c19cd1575e
Imran <md.imransyam1956@gmail.com>
2024-08-10 17:13:14 +0600

    testing 5

--- src/controllers/ai.js
+++ src/controllers/ai.js
@@ -4,21 +4,12 @@ import { run } from "../../geminiFlash.js";
 
 export const askGemini = async (req, res) => {
   try {
-    // return res.status(200).json({
-    //   name: "Imran",
-    //   age: 19
-    // })
     const { image } = req.body;
     if (!image) return res.sendStatus(401);
 
     const result = await run(image);
+    return res.status(200).json(result.slice(7, -3)).end();
     
-    return res.status(200).json({
-      name: "Imran",
-      result: result
-    })
-    // return res.status(200).json(result.slice(7, -3)).end();
-
     // return res;
     // .then((user) => {
     //   getAuth().setCustomUserClaims(user.uid, { isAdmin: isAdmin });

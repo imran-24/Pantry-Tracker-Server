c6db5a3e568ab18d8beb88459d6646a1bd9a566e
Imran <md.imransyam1956@gmail.com>
2024-08-10 23:21:00 +0600

    last commit

--- geminiFlash.js
+++ geminiFlash.js
@@ -8,12 +8,19 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 export async function run(base64Image) {
   try {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
     const prompt = `Provide the name and count of the object in this image. Ensure the response is in valid JSON format where there can't be any word specifically rentioned anything like json, with the correct object name and count, structured as {"object": "object_name", "count": number} without any extra information.`;
-    const imageParts = [base64Image.substring(base64Image.indexOf(",") + 1)];
 
-    const result = await model.generateContent([prompt, ...imageParts]);
+    const result = await model.generateContent([
+      prompt,
+      {
+        inlineData: {
+          data: base64Image.substring(base64Image.indexOf(",") + 1),
+          mimeType: "image/jpeg",
+        },
+      },
+    ]);
 
+    // console.log(result.response.text());
     return result.response.text();
   } catch (error) {
     console.error("An error occurred:", error);
--- src/controllers/ai.js
+++ src/controllers/ai.js
@@ -4,22 +4,12 @@ import { run } from "../../geminiFlash.js";
 
 export const askGemini = async (req, res) => {
   try {
-    // return res.status(200).json({
-    //   name: "Imran",
-    //   age: 19
-    // })
     const { image } = req.body;
     if (!image) return res.sendStatus(401);
 
     const result = await run(image);
+    return res.status(200).json(result).end();
     
-    console.log(result);
-    return res.status(200).json({
-      name: "Imran",
-      result: result.slice(7, -3),
-    }).end();
-    // return res.status(200).json(result.slice(7, -3)).end();
-
     // return res;
     // .then((user) => {
     //   getAuth().setCustomUserClaims(user.uid, { isAdmin: isAdmin });
